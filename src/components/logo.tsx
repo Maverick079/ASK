995b97111298ccc74c82839d139ceb278063aec3
Vijay Iyer <vijayiyer07@gmail.com>
2025-09-01 19:23:01 +0000

    Update this logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,36 +1,85 @@
 export function Logo({ className }: { className?: string }) {
     return (
       <div className={className}>
-        <svg viewBox="0 0 160 80" xmlns="http://www.w3.org/2000/svg" className="h-full w-auto">
-            <path d="M48 0L96 24H0Z" fill="black" transform="translate(24)"/>
-            <path d="M0 20 H160 V80 H0 Z" fill="#EE3339"/>
-            <text
-                x="50%"
-                y="50%"
-                dominantBaseline="middle"
-                textAnchor="middle"
-                fill="white"
-                fontSize="38"
-                fontWeight="900"
-                fontFamily="sans-serif"
-                >
-                ASK
-            </text>
-            <text
-                x="50%"
-                y="78%"
-                dominantBaseline="middle"
-                textAnchor="middle"
-                fill="white"
-                fontSize="12"
-                fontWeight="500"
-                letterSpacing="0.5"
-                fontFamily="sans-serif"
-            >
-                BRANDS &amp; SIGNS
-            </text>
+        <svg viewBox="0 0 200 160" xmlns="http://www.w3.org/2000/svg" className="h-full w-auto">
+          {/* ASK */}
+          <text 
+            x="100" 
+            y="25" 
+            fontFamily="'Poppins', sans-serif" 
+            fontSize="40" 
+            fontWeight="900" 
+            fill="#E52321" 
+            textAnchor="middle"
+            fontStyle="italic"
+          >
+            ASK
+          </text>
+          
+          {/* BRANDS & SIGNS */}
+          <text 
+            x="100" 
+            y="42" 
+            fontFamily="'Poppins', sans-serif" 
+            fontSize="14" 
+            fontWeight="700" 
+            fill="#004A99" 
+            textAnchor="middle"
+          >
+            BRANDS &amp; SIGNS
+          </text>
+          
+          {/* In associate with */}
+          <text 
+            x="100" 
+            y="65" 
+            fontFamily="'PT Sans', sans-serif" 
+            fontSize="12" 
+            fill="#333" 
+            textAnchor="middle"
+            fontStyle="italic"
+          >
+            In associate with
+          </text>
+
+          {/* Stars */}
+          <g transform="translate(100, 85) scale(0.8)">
+            <g transform="translate(-40, 0) rotate(-15)">
+              <text fontSize="12" fill="#FAD201" transform="translate(0, 2)">★</text>
+              <text fontSize="12" fill="#F9A825" transform="translate(12, -1)">★</text>
+              <text fontSize="12" fill="#F57C00" transform="translate(24, -3)">★</text>
+              <text fontSize="12" fill="#E65100" transform="translate(36, -4)">★</text>
+              <text fontSize="12" fill="#D50000" transform="translate(48, -4)">★</text>
+              <text fontSize="12" fill="#C40000" transform="translate(60, -3)">★</text>
+            </g>
+          </g>
+          
+          {/* URMIADS */}
+          <text 
+            x="100" 
+            y="110" 
+            fontFamily="'Poppins', sans-serif" 
+            fontSize="36" 
+            fontWeight="900" 
+            fill="#E52321" 
+            textAnchor="middle"
+          >
+            URMIADS
+          </text>
+          
+          {/* Advertising | Designing | Printing */}
+          <text 
+            x="100" 
+            y="125" 
+            fontFamily="'PT Sans', sans-serif" 
+            fontSize="10" 
+            fill="#333" 
+            textAnchor="middle" 
+            letterSpacing="0.5"
+          >
+            Advertising | Designing | Printing
+          </text>
         </svg>
       </div>
     );
   }
-  
